9f2fb2b9d7bc835b8080b6f8e8d2abb1069dc7a3
Ravi Lal <arelra@users.noreply.github.com>
2021-12-10 16:55:44 +0000

    use truncate option

--- index.js
+++ index.js
@@ -4,11 +4,16 @@ const truncateString = (str) =>
 const truncateArray = (arr) =>
     arr.length >= 10 ? arr.slice(0,9).concat("...") : arr
 
-const sortKeysAndTruncateValues = (obj) =>
+const sortKeysAndTruncateValues = (obj, truncate) =>
     Object.keys(obj).sort((a,b) => a.localeCompare(b)).reduce((result, key) => {
         const value = obj[key];
-        result[key] = typeof value === "string" ? truncateString(value)
+        if (truncate) {
+            result[key] = typeof value === "string"
+                        ? truncateString(value)
                         : Array.isArray(value) ? truncateArray(value) : value;
+        } else {
+            result[key] = value;
+        }
         return result;
     }, {});
 
@@ -42,7 +47,7 @@ const parseIfDefined = (obj, key, parseFn) => {
     return undefined;
 }
 
-const parseYouTubeRequest = (request) => {
+const parseYouTubeRequest = (request, truncate) => {
     let requestSummary = {};
 
     getRequestParams(request).forEach(rp => {
@@ -52,20 +57,20 @@ const parseYouTubeRequest = (request) => {
             parseIfDefined(
                 embedConfig?.adsConfig?.adTagParameters,
                 'cust_params',
-                (obj) => sortKeysAndTruncateValues(parseCustParams(obj))
+                (obj) => sortKeysAndTruncateValues(parseCustParams(obj), truncate)
             )
             parseIfDefined(
                 embedConfig?.adsConfig,
                 'adTagParameters',
-                (obj) => sortKeysAndTruncateValues(obj)
+                (obj) => sortKeysAndTruncateValues(obj, truncate)
             )
-            requestSummary["embed_config"] = sortKeysAndTruncateValues(embedConfig);
+            requestSummary["embed_config"] = sortKeysAndTruncateValues(embedConfig, truncate);
         } else {
             requestSummary[rp.substring(0, equalIndex)] = rp.substring(equalIndex+1);
         }
     });
 
     requestSummary["domain"] = getDomain(request);
-    requestSummary = sortKeysAndTruncateValues(requestSummary);
+    requestSummary = sortKeysAndTruncateValues(requestSummary, truncate);
     return JSON.stringify(requestSummary, null ,2);
 }
