a5ea8c41eecb0e00aa223cd8b1356dc28f1bc984
Ravi Lal <arelra@users.noreply.github.com>
2021-12-16 13:11:37 +0000

    Add tabs for output panes

--- src/Output.jsx
+++ src/Output.jsx
@@ -1,44 +1,56 @@
 import ReactDiffViewer from 'react-diff-viewer';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import './Output.css'
 
 function Output({ parsed: [ parsed1, parsed2 ] }) {
     console.log('hello', parsed1, parsed2);
     return (
         <div className="output-container">
-            <hr />
-            <div className="output-request-container">
-                <div>1</div>
-                <textarea
-                    className="output"
-                    id="output-request-1"
-                    rows="10"
-                    cols="50"
-                    value={parsed1}
-                >   
-                </textarea>
-                <div onClick={() => {navigator.clipboard.writeText(parsed1)}}>📋</div>
-            </div>
-            <div className="output-request-container">
-                <div>2</div>
-                <textarea
-                    className="output"
-                    id="output-2"
-                    rows="10"
-                    cols="50"
-                    value={parsed2}
-                >
-                </textarea>
-                <div onClick={() => {navigator.clipboard.writeText(parsed2)}}>📋</div>
-            </div>
-            <div className="diff-container">
-                <hr />
-                <ReactDiffViewer
-                    oldValue={parsed1}
-                    newValue={parsed2}
-                    splitView={false}
-                    showDiffOnly={true}
-                />
-            </div>
+            <Tabs>
+                <TabList>
+                    <Tab>Parsed 1</Tab>
+                    <Tab>Parsed 2</Tab>
+                    <Tab>Diff</Tab>
+                </TabList>
+
+                <TabPanel>
+                    <div className="output-request-container">
+                        <textarea
+                            className="output"
+                            id="output-request-1"
+                            rows="10"
+                            cols="50"
+                            value={parsed1}
+                        >   
+                        </textarea>
+                        <div onClick={() => {navigator.clipboard.writeText(parsed1)}}>📋</div>
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className="output-request-container">
+                        <textarea
+                            className="output"
+                            id="output-2"
+                            rows="10"
+                            cols="50"
+                            value={parsed2}
+                        >
+                        </textarea>
+                        <div onClick={() => {navigator.clipboard.writeText(parsed2)}}>📋</div>
+                    </div>
+                </TabPanel>
+                <TabPanel>
+                    <div className="diff-container">
+                        <ReactDiffViewer
+                            oldValue={parsed1}
+                            newValue={parsed2}
+                            splitView={false}
+                            showDiffOnly={true}
+                        />
+                    </div>
+                </TabPanel>
+            </Tabs>
         </div>
     )
 }
