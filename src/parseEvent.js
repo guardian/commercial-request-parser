feedf1ecb7c9fa5ac57fd63c78d0d865b94e29a7
Ravi Lal <arelra@users.noreply.github.com>
2022-01-22 17:36:09 +0000

    extract parse request function

--- src/parseEvent.js
+++ src/parseEvent.js
@@ -3,28 +3,29 @@ import {
     parseYouTubeRequest,
 } from './parser'
 
+function parseRequest(request, isYouTube, truncateValues, ignoreValues) {
+    if (request && request.trim().length > 0) {
+        if (isYouTube) {
+            return parseYouTubeRequest(request, truncateValues, ignoreValues)
+        } else {
+            return parseGAMRequest(request, truncateValues, ignoreValues);  
+        }
+    }
+    return "";
+}
+
 function parseEvent(event) {
     event.preventDefault();
     const request1 = [...event.target.elements].filter(t => t.id === "request-1")[0].value;
     const request2 = [...event.target.elements].filter(t => t.id === "request-2")[0].value;
     const truncateValues = [...event.target.elements].filter(t => t.id === "radio_truncate_values")[0].checked;
     const ignoreValues = [...event.target.elements].filter(t => t.id === "radio_ignore_values")[0].checked;
-    const youtube = [...event.target.elements].filter(t => t.id === "radio_type_youtube")[0].checked;
+    const isYouTube = [...event.target.elements].filter(t => t.id === "radio_type_youtube")[0].checked;
 
-    let parsed1 = "";
-    let parsed2 = "";
-
-    if (request1 && request1.trim().length > 0) {
-        parsed1 = youtube
-            ? parseYouTubeRequest(request1, truncateValues, ignoreValues)
-            : parseGAMRequest(request1, truncateValues, ignoreValues);
-    }
-    if (request2 && request2.trim().length > 0) {
-        parsed2 = youtube
-        ? parseYouTubeRequest(request2, truncateValues, ignoreValues)
-        : parseGAMRequest(request2, truncateValues, ignoreValues);
-    }
-    return [parsed1, parsed2]
+    return [
+        parseRequest(request1, isYouTube, truncateValues, ignoreValues),
+        parseRequest(request2, isYouTube, truncateValues, ignoreValues),
+    ];
 }
 
 export default parseEvent
